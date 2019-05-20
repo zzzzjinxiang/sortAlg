76f080fa1fe16d8062724ff92cb39c770119885a
zzzjx <zhaojingxiang@bupt.edu.cn>
2019-05-20 18:13:18 +0800

    InsertSortAlg commit

--- src/com/company/straightInsertSortMethod.java
+++ src/com/company/straightInsertSortMethod.java
@@ -2,23 +2,17 @@ package com.company;
 
 public class straightInsertSortMethod {
     //选取一个有序表，一个无序表
-    public void sorted(Integer[] arrays){
-//        if(arrays.length<2)
-//            return;
-        int len = arrays.length-1;
-        int first;
-        int first2;
-        int move;
-        for(first2=1;first2<=len;first2++){
-            for(first=first2-1;first>=0;first--){
-                if(arrays[first]<arrays[first2])
-                    break;
+    public void sorted(int[] arr){
+        int len = arr.length;
+        int i,j,key;
+        for(i=1; i<len;i++){
+            key=arr[i];
+            j=i-1;
+            while(arr[j]>key && j>=0){
+                arr[j+1] = arr[j];
+                j--;
             }
-            int temp = arrays[first2];
-            for(move=first2-1;move>=first+1;move--){
-                arrays[move+1]=arrays[move];
-            }
-            arrays[first+1]=temp;
+            arr[j+1]=key;
         }
     }
 }
